13624c4bb827e987292453a6c68b44bb0879acb8
Ezy <ezra@passionfruit.co.nz>
2021-06-21 15:40:44 +1200

    retweets work

--- src/api/Tweet/retweet/toggleRetweet.js
+++ src/api/Tweet/retweet/toggleRetweet.js
@@ -6,7 +6,9 @@ module.exports = {
       if (!userId) throw Error("You need to be authenticated");
 
       // 2. make sure the tweet actually exists
-      const tweet = await ctx.prisma.tweet({ id: args.id });
+      const tweet = await ctx.prisma.tweet.findFirst({
+        where: { id: args.id },
+      });
       if (!tweet) throw Error(`No tweet exists for id - ${args.id}`);
 
       // 3. make sure the tweet is someone else
@@ -19,28 +21,28 @@ module.exports = {
 
       // 4. remove the retweet if it already exists
       // otherwise, retweeeeeeet
-      const retweetExists = await ctx.prisma.retweets({
+      const retweetExists = await ctx.prisma.retweet.findFirst({
         where: {
           AND: [{ tweet: { id: args.id } }, { user: { id: userId } }],
         },
       });
 
-      if (retweetExists.length) {
-        await ctx.prisma.deleteRetweet({ id: retweetExists[0].id });
+      if (retweetExists) {
+        await ctx.prisma.retweet.delete({ where: { id: retweetExists[0].id } });
         return true;
       } else {
-        await ctx.prisma.createRetweet({
-          user: {
-            connect: { id: userId },
-          },
-          tweet: {
-            connect: { id: args.id },
+        await ctx.prisma.retweet.create({
+          data: {
+            user: {
+              connect: { id: userId },
+            },
+            tweet: {
+              connect: { id: args.id },
+            },
           },
         });
         return true;
       }
-
-      return false;
     },
   },
 };
